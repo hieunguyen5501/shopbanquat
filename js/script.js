5efa6618712503c0a1d0157662f59971f5452002
DUC-ANH-GHEL <aduc7246@gmail.com>
2020-05-18 15:37:36 +0700

    5/18/2020 3:37 PM

--- js/script.js
+++ js/script.js
@@ -1,15 +1,16 @@
-$(document).ready(function(){
-	$('.menu-item').on('click', function() {
+$(document).ready(function () {
+	$('.menu-item').on('click', function () {
 		$('.menu-item').removeClass('active');
 		$(this).addClass('active');
-		
+
 	});
 
-	$('.menu-open').on('click', function() {
-		$('.menu-responsive.hidden-lg').addClass('show-menu-responsive');
+	$('.menu-open').on('click', function () {
+		console.log(1);
+		$('.menu-responsive').addClass('show-menu-responsive');
 	});
 
-	$('.menu-close').on('click', function() {
-		$('.menu-responsive.hidden-lg').removeClass('show-menu-responsive');
+	$('.menu-close').on('click', function () {
+		$('.menu-responsive').removeClass('show-menu-responsive');
 	})
 });
